5181f12ef0e2d0de9c5f44a2d96b49be200cf95f
meroua <merouakemoum@hotmail.com>
2023-09-08 02:45:23 +0000

    add C program that prints with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   * Description: This program uses the printf function to print a message.
+ *    * Return: Always return 0 (success)
+ *     **/
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
