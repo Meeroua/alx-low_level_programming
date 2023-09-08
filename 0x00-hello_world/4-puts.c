05500738e80669f802078343d3507471ee5bb6e1
Meeroua <66382685+Meeroua@users.noreply.github.com>
2023-09-08 03:41:52 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *  * main - Entry point
- *   * Description: This program uses the puts function to print a message.
- *    * Return: Always return 0 (success)
- *     */
+ * main - Entry point
+ * Description: This program uses the puts function to print a message.
+ * Return: Always return 0 (success)
+ **/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
