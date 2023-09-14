a0b17613ad1692d5790f6165cfea32c9e909c749
Meeroua <66382685+Meeroua@users.noreply.github.com>
2023-09-14 02:28:01 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- ** print_alphabet - function that prints the alphabet
- ** Description: 0x02. C - task 2
- ** Return: alphabet
+ * print_alphabet - function that prints the alphabet
+ * Description: 0x02. C - task 2
+ * Return: alphabet
  */
 void print_alphabet(void)
 {
-    	char c;
-    	c = 'a';
-	while (c <= 'z')
-	{
-		_putchar(c);
-		c++;
-	}
-	_putchar('\n');
+    char c;
+    c = 'a';
+    while (c <= 'z')
+    {
+        _putchar(c);
+        c++;
+    }
+    _putchar('\n');
 }
