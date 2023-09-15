caebc95598fbc1f5270a22db50cebbe0ad285dbd
meroua <merouakemoum@hotmail.com>
2023-09-15 00:57:04 +0000

    add function that multiplies two integers.

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _isupper - checks for a digit
+ * _isdigit - checks for a digit
  * Description: 0x04. C - task 1
  * @c: input character
  * Return: 1 if c is a digit; 0 otherwise
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/*
+ *  mul - multiplies two integers
+ *  @x: input number
+ *  @y: input number
+ *  Return: product of multiplication
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
