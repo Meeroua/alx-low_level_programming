a9fa51f29fc31b9e70f1a645ec3e77c0657a21af
meroua <merouakemoum@hotmail.com>
2023-09-26 21:53:05 +0000

    add task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+/**
+ *  _strlen - function that calculates length of a string
+ *   @s: string address
+ *    Return: length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
