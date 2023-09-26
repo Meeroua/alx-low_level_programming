1456a2468e7ff093f611baec6c6cd270710ab750
meroua <merouakemoum@hotmail.com>
2023-09-26 22:17:17 +0000

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  puts2 -  prints every other character of a string
+ *   @str: string address
+ */
+void puts2(char *str)
+{
+	int n, i;
+	n = 0;
+	while (str[n] != '\0')
+	{
+		n++;
+	}
+	for (i = 0; i < n; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
