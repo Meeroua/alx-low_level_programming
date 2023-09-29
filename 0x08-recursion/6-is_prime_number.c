f22617dfbdc7fff4204c2e7683daa5ad30a54dec
meroua <merouakemoum@hotmail.com>
2023-09-29 01:34:53 +0000

    update task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,11 +10,9 @@ int check_prime(int n, int i)
 {
 	if (i > n / 2)
 		return (1);
-	else
-		if (n % i == 0)
-			return (0);
-		else
-			return (check_prime(n, i + 1));
+	if (n % i == 0)
+		return (0);
+	return (check_prime(n, i + 1));
 }
 
 /**
@@ -26,6 +24,5 @@ int is_prime_number(int n)
 {
 	if (n <= 0 || n == 1)
 		return (0);
-	else
-		return (check_prime(n, 2));
+	return (check_prime(n, 2));
 }
