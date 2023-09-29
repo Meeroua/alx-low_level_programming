1da6f4c9f35b6506076b029b1a938dc56f0460d0
meroua <merouakemoum@hotmail.com>
2023-09-29 01:28:06 +0000

    add task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *  check_prime - checks if the input is a prime number
+ *   @n: input number
+ *    @i: current guess for a factor
+ *     Return: 1 if the input is a prime number, 0 otherwise.
+ */
+int check_prime(int n, int i)
+{
+	if (i > n / 2)
+		return (1);
+	else
+		if (n % i == 0)
+			return (0);
+		else
+			return (check_prime(n, i + 1));
+}
+
+/**
+ *  is_prime_number - checks if the input is a prime number
+ *   @n: input number
+ *    Return: 1 if the input is a prime number, 0 otherwise.
+ */
+int is_prime_number(int n)
+{
+	if (n <= 0 || n == 1)
+		return (0);
+	else
+		return (check_prime(n, 2));
+}
