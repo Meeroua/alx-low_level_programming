b195ca381f7be560227d3d68f208c6921f36e1e7
meroua <merouakemoum@hotmail.com>
2023-10-25 18:45:17 +0000

    add task 0 - 0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *  main - task 0
+ *   @argc: number of arguments
+ *    @argv: array containing arguments
+ *     Return: 0
+ */
+int main(int argc, char *argv[]){
+	if (argc > 0) {
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
