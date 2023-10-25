8fe41e897cf320381825c95fc3e84c1ffeb5384b
meroua <merouakemoum@hotmail.com>
2023-10-25 17:49:28 +0000

    update functions

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * Empty
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (dest);
+}
--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,19 +1,10 @@
-#include "main.h"
+#include <unistd.h>
+
 /**
- ** main - beginning
- ** Description: 0x02. C - task 1
- ** Return: 0
- */
-int main(void)
+ *  * _putchar - writes character to stdout
+ *   * @c : character to print
+ *    */
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+		return (write(1, &c, 1));
 }
