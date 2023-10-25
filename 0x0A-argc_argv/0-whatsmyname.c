99209418f1fb7c0f6d7a9e6561c3f404e4a647ca
meroua <merouakemoum@hotmail.com>
2023-10-25 18:47:52 +0000

    update betty style

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,9 +5,10 @@
  *    @argv: array containing arguments
  *     Return: 0
  */
-int main(int argc, char *argv[]){
-	if (argc > 0) {
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
 		printf("%s\n", argv[0]);
-	}
+
 	return (0);
 }
